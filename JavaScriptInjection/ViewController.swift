b8b0a02e771491cae181bb5825010cea5d39d2e5
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-17 17:48:54 -0300

    Initial Commit

---
+++ JavaScriptInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavaScriptInjection
+//
+//  Created by Rodrigo Cavalcanti on 17/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
