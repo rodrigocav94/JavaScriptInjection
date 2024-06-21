b30ebf679ffe9910bbcd1926a69501fbe03e80ac
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-21 03:47:50 -0300

    Used NotificationCenter to to add a scroll indicator to text view when a keyboard is showing

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -37,9 +37,19 @@ class ActionViewController: UIViewController {
         }
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        let notificationCenter = NotificationCenter.default // Get a reference to the default notification center.
+        notificationCenter.addObserver(
+            self, // The object that should receive notifications (it's self)
+            selector: #selector(adjustForKeyboard), // The method that should be called
+            name: UIResponder.keyboardWillHideNotification, // The notification we want to receive
+            object: nil // The object we want to watch. Nil means "we don't care who sends the notification."
+        )
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
-    @IBAction func done() {
+    @objc func done() {
         let argument: NSDictionary = ["customJavaScript": script.text as Any]
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: UTType.propertyList.identifier as String)
@@ -49,5 +59,28 @@ class ActionViewController: UIViewController {
         
         extensionContext?.completeRequest(returningItems: [item])
     }
-
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return } // UIResponder.keyboardFrameEndUserInfoKey tells us the frame of the keyboard after it has finished animating. This will be of type NSValue, which in turn is of type CGRect. The CGRect struct holds both a CGPoint and a CGSize, so it can be used to describe a rectangle.
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue // pulling out the correct frame of the keyboard
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window) // Converting the rectangle to our view's co-ordinates. Convert() is used to be make sure it works on landscape too.
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero //  workaround for hardware keyboards being connected by explicitly setting the insets to be zero.
+        } else {
+            script.contentInset = UIEdgeInsets(
+                top: 0,
+                left: 0,
+                bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, //  indent the edges of our text view so that it appears to occupy less space even though its constraints are still edge to edge in the view.
+                right: 0
+            )
+        }
+        
+        // scroll so that the text entry cursor is visible. If the text view has shrunk this will now be off screen, so scrolling to find it again keeps the user experience intact.
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
 }
